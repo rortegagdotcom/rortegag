774d193a01d88331d970faadadd4f60e27924d1a
r-ortegag <raulortegagallego48@hotmail.es>
2022-01-19 17:40:39 +0100

    Minificado el codigo Javascript

--- js/enhance.js
+++ js/enhance.js
@@ -1,11 +1,1 @@
-if(window.innerWidth < 435) {
-    console.log(document.getElementById('home').innerHTML = '<i class="bi bi-house-door-fill fs-2"></i>');
-    console.log(document.getElementById('aboutme').innerHTML = '<i class="bi bi-person-fill fs-2"></i>');
-    console.log(document.getElementById('blog').innerHTML = '<i class="bi bi-card-heading fs-2"></i>');
-    console.log(document.getElementById('contact').innerHTML = '<i class="bi bi-envelope-fill fs-2"></i>');
-} else {
-    console.log(document.getElementById('home').innerHTML = '<i class="bi bi-house-door-fill"></i>  Inicio</a>');
-    console.log(document.getElementById('aboutme').innerHTML = '<i class="bi bi-person-fill"></i>  Sobre mí</a>');
-    console.log(document.getElementById('blog').innerHTML = '<i class="bi bi-card-heading"></i>  Blog</a>');
-    console.log(document.getElementById('contact').innerHTML = '<i class="bi bi-envelope-fill"></i>  Contacto</a>');
-}
+window.innerWidth<435?(document.getElementById("home").innerHTML='<i class="bi bi-house-door-fill fs-2"></i>',document.getElementById("aboutme").innerHTML='<i class="bi bi-person-fill fs-2"></i>',document.getElementById("blog").innerHTML='<i class="bi bi-card-heading fs-2"></i>',document.getElementById("contact").innerHTML='<i class="bi bi-envelope-fill fs-2"></i>'):(document.getElementById("home").innerHTML='<i class="bi bi-house-door-fill"></i>  Inicio</a>',document.getElementById("aboutme").innerHTML='<i class="bi bi-person-fill"></i>  Sobre mí</a>',document.getElementById("blog").innerHTML='<i class="bi bi-card-heading"></i>  Blog</a>',document.getElementById("contact").innerHTML='<i class="bi bi-envelope-fill"></i>  Contacto</a>');
